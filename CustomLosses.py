92eea998ec556848317b7734a11019cbaa1ffe30
mpkuse <mpkuse@connect.ust.hk>
2018-11-06 11:46:15 +0800

    moved all my custom loss function and eval metrics into CustomLosses.py

---
+++ CustomLosses.py
@@ -0,0 +1,161 @@
+"""
+    Contains my implementation of custom losses / validation functions.
+    Works with keras2.0 and tf1.11.
+
+        Author  : Manohar Kuse <mpkuse@connect.ust.hk>
+        Created : 6th Nov, 2018
+"""
+
+from keras import backend as K
+from keras.engine.topology import Layer
+import keras
+
+import numpy as np
+#import cv2
+import code
+
+
+def triplet_loss2_maker( nP, nN, epsilon=0.3 ):
+
+    # As per the NetVLAD paper's words
+    def triplet_loss2( y_true, y_pred ):
+    # def triplet_loss2( params ):
+        # y_true, y_pred = params
+        """ Closed negative sample - farthest positive sample """
+        assert( y_pred.shape[1] == 1+nP+nN )
+
+        # y_pred.shape = shape=(?, 5, 512)
+        q = y_pred[:,0:1,:]    # shape=(?, 1, 512)
+        P = y_pred[:,1:1+nP,:] # shape=(?, nP, 512)
+        N = y_pred[:,1+nP:,:]  # shape=(?, nN, 512)
+
+        q_dot_P = keras.layers.dot( [q,P], axes=-1 )  # shape=(?, 1, nP)
+        q_dot_N = keras.layers.dot( [q,N], axes=-1 )  # shape=(?, 1, nN)
+
+
+        # epsilon = 0.3  # Your epsilon here
+
+        d_nearest_positive_sample = K.max( q_dot_P, axis=-1, keepdims=True )
+        S = q_dot_N - d_nearest_positive_sample + epsilon #difference between best +ve and all negatives.
+        return K.sum( K.maximum( 0., S ), axis=-1 )
+
+    return triplet_loss2
+
+
+
+def allpair_hinge_loss_maker( nP, nN, epsilon=0.3 ):
+    # def allpair_hinge_loss( params ):
+        # y_true, y_pred = params
+    def allpair_hinge_loss(y_true, y_pred):
+        """ All pair loss """
+        # nP = 3
+        # nN = 2
+        assert( y_pred.shape[1] == 1+nP+nN )
+
+        # y_pred.shape = shape=(?, 5, 512)
+        q = y_pred[:,0:1,:]    # shape=(?, 1, 512)
+        P = y_pred[:,1:1+nP,:] # shape=(?, 2, 512)
+        N = y_pred[:,1+nP:,:]  # shape=(?, 2, 512)
+
+        q_dot_P = keras.layers.dot( [q,P], axes=-1 )  # shape=(?, 1, 2)
+        q_dot_N = keras.layers.dot( [q,N], axes=-1 )  # shape=(?, 1, 2)
+
+        # epsilon = 0.3  # Your epsilon here
+
+        zeros = K.zeros((nP, nN), dtype='float32')
+        ones_m = K.ones((nP,1), dtype='float32')
+        ones_n = K.ones((nN,1), dtype='float32')
+
+
+        _1m__qdotN_T = ones_m[None,:] * q_dot_N # 1m ( \delta^q_N )^T
+        qdotP__1n_T = K.permute_dimensions( ones_n[None,:] * q_dot_P, [0,2,1] ) # ( \delta^q_P ) 1n^T
+        _1m__1n_T = epsilon * ones_m[None,:] * K.permute_dimensions( ones_n[None,:], [0,2,1] ) # 1m 1n^T
+
+        aux = _1m__qdotN_T - qdotP__1n_T + _1m__1n_T
+
+        return K.sum( K.maximum(zeros, aux) , axis=[-1,-2] )
+    return allpair_hinge_loss
+
+
+
+def allpair_count_goodfit_maker( nP, nN, epsilon=0.3 ):
+    # def allpair_count_goodfit( params ):
+        # y_true, y_pred = params
+    def allpair_count_goodfit(y_true, y_pred):
+        # nP = 3
+        # nN = 2
+        assert( y_pred.shape[1] == 1+nP+nN )
+
+        # y_pred.shape = shape=(?, 5, 512)
+        q = y_pred[:,0:1,:]    # shape=(?, 1, 512)
+        P = y_pred[:,1:1+nP,:] # shape=(?, 2, 512)
+        N = y_pred[:,1+nP:,:]  # shape=(?, 2, 512)
+
+        q_dot_P = keras.layers.dot( [q,P], axes=-1 )  # shape=(?, 1, 2)
+        q_dot_N = keras.layers.dot( [q,N], axes=-1 )  # shape=(?, 1, 2)
+
+        # epsilon = 0.3  # Your epsilon here
+
+        zeros = K.zeros((nP, nN), dtype='float32')
+        ones_m = K.ones((nP,1), dtype='float32')
+        ones_n = K.ones((nN,1), dtype='float32')
+
+        _1m__qdotN_T = ones_m[None,:] * q_dot_N # 1m ( \delta^q_N )^T
+        qdotP__1n_T = K.permute_dimensions( ones_n[None,:] * q_dot_P, [0,2,1] ) # ( \delta^q_P ) 1n^T
+        _1m__1n_T = epsilon * ones_m[None,:] * K.permute_dimensions( ones_n[None,:], [0,2,1] ) # 1m 1n^T
+
+        aux = _1m__qdotN_T - qdotP__1n_T + _1m__1n_T
+
+        return K.sum( K.cast( K.less_equal( aux , 0),  'float32' ), axis=[-1,-2] ) #number of pairs which satisfy out of total nP*nN pairs
+    return allpair_count_goodfit
+
+
+# Verify loss function
+if __name__ == '__main__':
+    np.random.seed(0)
+    nP = 3
+    nN = 2
+    y_true = keras.layers.Input( shape=(nP+nN+1,7) )
+    y_pred = keras.layers.Input( shape=(nP+nN+1,7) )
+
+
+    w = keras.layers.Lambda( triplet_loss2_maker( nP=nP, nN=nN, epsilon=0.1) )( [y_true, y_pred] )
+    # w = keras.layers.Lambda( allpair_hinge_loss_maker( nP=nP, nN=nN, epsilon=0.1) )( [y_true, y_pred] )
+    w_c = keras.layers.Lambda( allpair_count_goodfit_maker( nP=nP, nN=nN, epsilon=0.1) )( [y_true, y_pred] )
+    model = keras.models.Model( inputs=[y_true,y_pred], outputs=[w, w_c] )
+
+    model.summary()
+    keras.utils.plot_model( model, show_shapes=True )
+
+    a = np.zeros( (10,6,7) ) #. this doesn't appear in the loss function as my loss functions are weakly supervised. don't care, this is y_true
+    b_ = np.zeros( (10,6,7) )
+
+    b = b_[0,:,:]
+    b = np.round( np.random.random( (6,7) ), 2)
+    b = b / np.linalg.norm( b, axis=1, keepdims=True )
+    # b[0,0] = (3./5)
+    # b[0,6] = (4./5)
+    #
+    # b[1,0] = (3./5)
+    # b[1,1] = (4./5)
+    #
+    # b[2,0] = (3./5)
+    # b[2,3] = (4./5)
+    #
+    # b[2,0] = (3./5)
+    # b[2,3] = (4./5)
+    #
+    # b[2,0] = (3./5)
+    # b[2,3] = (4./5)
+
+    # b[1:3,:] = np.round( np.random.random( (2,7)), 2 )
+    # b[3:,:] = np.round( np.random.random( (2,7)), 2 )
+    b_[0,:,:] = b
+    b_[2,:,:] = b
+
+    out = model.predict( [a,b_] )
+
+    aux = np.array( [[-0.05192798, -0.00773406],
+       [ 0.1755529 ,  0.21974683],
+       [ 0.06959844,  0.11379236]])
+    quit()
--- noveou_train_netvlad_v3.py
+++ noveou_train_netvlad_v3.py
@@ -24,6 +24,9 @@ from CustomNets import NetVLADLayer
 from CustomNets import dataload_, do_typical_data_aug
 from CustomNets import make_from_mobilenet
 
+# CustomLoses
+from CustomLosses import triplet_loss2_maker, allpair_hinge_loss_maker, allpair_count_goodfit_maker
+
 from InteractiveLogger import InteractiveLogger
 
 
@@ -34,7 +37,8 @@ from WalksRenderer import WalksRenderer
 from PittsburgRenderer import PittsburgRenderer
 
 class WSequence(keras.utils.Sequence):
-    def __init__(self, nP, nN, n_samples=500 ):
+    """  This class depends on CustomNets.dataload_ for loading data. """
+    def __init__(self, nP, nN, n_samples=500, initial_epoch=0 ):
         # self.x, self.y = x_set, y_set
         self.D = dataload_( n_tokyoTimeMachine=n_samples, n_Pitssburg=-1, nP=nP, nN=nN )
         print 'dataload_ returned len(self.D)=', len(self.D), 'self.D[0].shape=', self.D[0].shape
@@ -43,7 +47,7 @@ class WSequence(keras.utils.Sequence):
         # Data Augmentation
         # self.D = do_typical_data_aug( self.D )
 
-        self.epoch = 0
+        self.epoch = initial_epoch
         self.batch_size = 4
         self.refresh_data_after_n_epochs = 20
         self.n_samples = n_samples
@@ -82,174 +86,6 @@ class WSequence(keras.utils.Sequence):
 
 
 
-def triplet_loss( y_true, y_pred ):
-# def triplet_loss( params ):
-    # y_true, y_pred = params
-    """ Closed negative sample - farthest positive sample """
-    assert( y_pred.shape[1] == 1+nP+nN )
-
-    # y_pred.shape = shape=(?, 5, 512)
-    q = y_pred[:,0:1,:]    # shape=(?, 1, 512)
-    P = y_pred[:,1:1+nP,:] # shape=(?, nP, 512)
-    N = y_pred[:,1+nP:,:]  # shape=(?, nN, 512)
-
-    q_dot_P = keras.layers.dot( [q,P], axes=-1 )  # shape=(?, 1, nP)
-    q_dot_N = keras.layers.dot( [q,N], axes=-1 )  # shape=(?, 1, nN)
-
-
-    epsilon = 0.3  # Your epsilon here
-    d_nearest_negative_sample =  K.max( q_dot_N, axis=-1 )
-    d_farthest_positive_sample = K.min( q_dot_P, axis=-1 )
-
-    # case-A: d_nearest_negative_sample > d_farthest_positive_sample
-    # Penalize this
-
-    # case-B: d_nearest_negative_sample < d_farthest_positive_sample
-    # This is desired, so this should have zero loss
-
-    return K.maximum( 0., d_nearest_negative_sample - d_farthest_positive_sample + epsilon )
-
-
-# As per the NetVLAD paper's words
-def triplet_loss2( y_true, y_pred ):
-# def triplet_loss2( params ):
-    # y_true, y_pred = params
-    """ Closed negative sample - farthest positive sample """
-    assert( y_pred.shape[1] == 1+nP+nN )
-
-    # y_pred.shape = shape=(?, 5, 512)
-    q = y_pred[:,0:1,:]    # shape=(?, 1, 512)
-    P = y_pred[:,1:1+nP,:] # shape=(?, nP, 512)
-    N = y_pred[:,1+nP:,:]  # shape=(?, nN, 512)
-
-    q_dot_P = keras.layers.dot( [q,P], axes=-1 )  # shape=(?, 1, nP)
-    q_dot_N = keras.layers.dot( [q,N], axes=-1 )  # shape=(?, 1, nN)
-
-
-    epsilon = 0.3  # Your epsilon here
-
-    d_nearest_positive_sample = K.max( q_dot_P, axis=-1, keepdims=True )
-    S = q_dot_N - d_nearest_positive_sample + epsilon #difference between best +ve and all negatives.
-    return K.sum( K.maximum( 0., S ), axis=-1 )
-
-
-
-
-# def allpair_hinge_loss( params ):
-    # y_true, y_pred = params
-def allpair_hinge_loss(y_true, y_pred):
-    """ All pair loss """
-    # nP = 3
-    # nN = 2
-    assert( y_pred.shape[1] == 1+nP+nN )
-
-    # y_pred.shape = shape=(?, 5, 512)
-    q = y_pred[:,0:1,:]    # shape=(?, 1, 512)
-    P = y_pred[:,1:1+nP,:] # shape=(?, 2, 512)
-    N = y_pred[:,1+nP:,:]  # shape=(?, 2, 512)
-
-    q_dot_P = keras.layers.dot( [q,P], axes=-1 )  # shape=(?, 1, 2)
-    q_dot_N = keras.layers.dot( [q,N], axes=-1 )  # shape=(?, 1, 2)
-
-    epsilon = 0.3  # Your epsilon here
-
-    zeros = K.zeros((nP, nN), dtype='float32')
-    ones_m = K.ones((nP,1), dtype='float32')
-    ones_n = K.ones((nN,1), dtype='float32')
-
-
-    _1m__qdotN_T = ones_m[None,:] * q_dot_N # 1m ( \delta^q_N )^T
-    qdotP__1n_T = K.permute_dimensions( ones_n[None,:] * q_dot_P, [0,2,1] ) # ( \delta^q_P ) 1n^T
-    _1m__1n_T = epsilon * ones_m[None,:] * K.permute_dimensions( ones_n[None,:], [0,2,1] ) # 1m 1n^T
-
-    aux = _1m__qdotN_T - qdotP__1n_T + _1m__1n_T
-
-    return K.sum( K.maximum(zeros, aux) , axis=[-1,-2] )
-
-
-# def allpair_count_goodfit( params ):
-    # y_true, y_pred = params
-def allpair_count_goodfit(y_true, y_pred):
-    # nP = 3
-    # nN = 2
-    assert( y_pred.shape[1] == 1+nP+nN )
-
-    # y_pred.shape = shape=(?, 5, 512)
-    q = y_pred[:,0:1,:]    # shape=(?, 1, 512)
-    P = y_pred[:,1:1+nP,:] # shape=(?, 2, 512)
-    N = y_pred[:,1+nP:,:]  # shape=(?, 2, 512)
-
-    q_dot_P = keras.layers.dot( [q,P], axes=-1 )  # shape=(?, 1, 2)
-    q_dot_N = keras.layers.dot( [q,N], axes=-1 )  # shape=(?, 1, 2)
-
-    epsilon = 0.3  # Your epsilon here
-
-    zeros = K.zeros((nP, nN), dtype='float32')
-    ones_m = K.ones((nP,1), dtype='float32')
-    ones_n = K.ones((nN,1), dtype='float32')
-
-    _1m__qdotN_T = ones_m[None,:] * q_dot_N # 1m ( \delta^q_N )^T
-    qdotP__1n_T = K.permute_dimensions( ones_n[None,:] * q_dot_P, [0,2,1] ) # ( \delta^q_P ) 1n^T
-    _1m__1n_T = epsilon * ones_m[None,:] * K.permute_dimensions( ones_n[None,:], [0,2,1] ) # 1m 1n^T
-
-    aux = _1m__qdotN_T - qdotP__1n_T + _1m__1n_T
-
-    return K.sum( K.cast( K.less_equal( aux , 0),  'float32' ), axis=[-1,-2] ) #number of pairs which satisfy out of total nP*nN pairs
-
-
-
-# Verify loss function
-if __name__ == '__1main__':
-    np.random.seed(0)
-    nP = 3
-    nN = 2
-    y_true = keras.layers.Input( shape=(6,7) )
-    y_pred = keras.layers.Input( shape=(6,7) )
-
-    # u = keras.layers.Lambda( allpair_hinge_loss )( [y_true, y_pred] )
-    # v = keras.layers.Lambda( allpair_count_goodfit )( [y_true, y_pred] )
-    w = keras.layers.Lambda( triplet_loss2 )( [y_true, y_pred] )
-    model = keras.models.Model( inputs=[y_true,y_pred], outputs=w )
-
-    model.summary()
-    keras.utils.plot_model( model, show_shapes=True )
-
-    a = np.zeros( (10,6,7) ) # don't care, this is y_true
-    b_ = np.zeros( (10,6,7) )
-
-    b = b_[0,:,:]
-    b = np.round( np.random.random( (6,7) ), 2)
-    b = b / np.linalg.norm( b, axis=1, keepdims=True )
-    # b[0,0] = (3./5)
-    # b[0,6] = (4./5)
-    #
-    # b[1,0] = (3./5)
-    # b[1,1] = (4./5)
-    #
-    # b[2,0] = (3./5)
-    # b[2,3] = (4./5)
-    #
-    # b[2,0] = (3./5)
-    # b[2,3] = (4./5)
-    #
-    # b[2,0] = (3./5)
-    # b[2,3] = (4./5)
-
-    # b[1:3,:] = np.round( np.random.random( (2,7)), 2 )
-    # b[3:,:] = np.round( np.random.random( (2,7)), 2 )
-    b_[0,:,:] = b
-    b_[2,:,:] = b
-
-    out = model.predict( [a,b_] )
-
-    aux = np.array( [[-0.05192798, -0.00773406],
-       [ 0.1755529 ,  0.21974683],
-       [ 0.06959844,  0.11379236]])
-    quit()
-
-
-
-
 # Training
 if __name__ == '__main__':
     image_nrows = 240
@@ -258,8 +94,9 @@ if __name__ == '__main__':
     # int_logr = InteractiveLogger( './models.keras/mobilenet_conv7_allpairloss/' )
     # int_logr = InteractiveLogger( './models.keras/mobilenet_conv7_tripletloss2/' )
 
-    int_logr = InteractiveLogger( './models.keras/mobilenet_conv7_quash_chnls_allpairloss/' )
-    # int_logr = InteractiveLogger( './models.keras/mobilenet_conv7_quash_chnls_tripletloss2/' )
+    # int_logr = InteractiveLogger( './models.keras/mobilenet_conv7_quash_chnls_allpairloss/' )
+    # int_logr = InteractiveLogger( './models.keras/mobilenet_conv7_quash_chnls_tripletloss2_K64/' )
+    int_logr = InteractiveLogger( './models.keras/test/' )
     nP = 6
     nN = 6
 
@@ -275,7 +112,7 @@ if __name__ == '__main__':
     cnn_dwn = keras.layers.normalization.BatchNormalization()( cnn_dwn )
     cnn = cnn_dwn
 
-    out, out_amap = NetVLADLayer(num_clusters = 16)( cnn )
+    out, out_amap = NetVLADLayer(num_clusters = 64)( cnn )
     model = keras.models.Model( inputs=input_img, outputs=out )
 
     # Plot
@@ -285,7 +122,8 @@ if __name__ == '__main__':
 
 
     # Load Previous Weights
-    # model.load_weights(  int_logr.dir() + '/core_model.keras' )
+    # model.load_weights(  int_logr.dir() + '/core_model.950.keras' )
+    initial_epoch = 0
 
 
 
@@ -300,23 +138,27 @@ if __name__ == '__main__':
 
     t_model.summary()
     keras.utils.plot_model( t_model, to_file=int_logr.dir()+'core_t.png', show_shapes=True )
+    print 'Write Directory : ', int_logr.dir()
+    # int_logr.fire_editor()
 
 
     #--------------------------------------------------------------------------
     # Compile
     #--------------------------------------------------------------------------
     sgdopt = keras.optimizers.Adadelta(  )
-    t_model.compile( loss=allpair_hinge_loss, optimizer=sgdopt, metrics=[allpair_count_goodfit] )
-    # t_model.compile( loss=triplet_loss, optimizer=sgdopt, metrics=[allpair_count_goodfit] ) #not in use. TODO removal
-    # t_model.compile( loss=triplet_loss2, optimizer=sgdopt, metrics=[allpair_count_goodfit] )
-    # int_logr.fire_editor()
+    loss = loss=triplet_loss2_maker(nP=nP, nN=nN, epsilon=0.3)
+    metrics = allpair_count_goodfit_maker( nP=nP, nN=nN, epsilon=0.3 )
+
+    # t_model.compile( loss=allpair_hinge_loss, optimizer=sgdopt, metrics=[allpair_count_goodfit] )
+    t_model.compile( loss=loss, optimizer=sgdopt, metrics=[metrics] )
+
 
     import tensorflow as tf
     tb = tf.keras.callbacks.TensorBoard( log_dir=int_logr.dir() )
 
 
-    t_model.fit_generator( generator=WSequence(nP, nN),
-                            epochs=1500, verbose=1, initial_epoch=0,
+    t_model.fit_generator( generator=WSequence(nP, nN, initial_epoch=initial_epoch),
+                            epochs=900, verbose=1, initial_epoch=initial_epoch,
                             validation_data = WSequence(nP, nN),
                             callbacks=[tb]
                          )
